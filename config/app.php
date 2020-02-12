d3e78a0c14bf3b12246896ac654d3f13e2bebf94
jmunozf96 <56194124+jmunozf96@users.noreply.github.com>
2020-02-11 22:37:42 -0500

    autenticacion

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \App\Providers\HelperProvider::class
+        \App\Providers\HelperProvider::class,
+        \App\Providers\JwtAuthServiceProvider::class
 
     ],
 
@@ -226,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Servicios' => \App\Helpers\Helper::class
+        'Servicios' => \App\Helpers\Helper::class,
+        'JwtAuth' => \App\Helpers\JwtAuth::class
 
     ],
 
