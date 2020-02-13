abf15d6300002856828e95da071ff7d2d7839e06
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2020-02-13 15:29:43 -0500

    MODULO EMPACADORA

--- app/Http/Controllers/Empacadora/EmpCajaController.php
+++ app/Http/Controllers/Empacadora/EmpCajaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Empacadora;
 use App\Http\Controllers\Controller;
 use App\Models\Empacadora\EMP_CAJA;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EmpCajaController extends Controller
 {
@@ -21,23 +22,77 @@ class EmpCajaController extends Controller
     {
         $cajas = EMP_CAJA::all()->load(['destino', 'distribuidor', 'tipo_caja']);
 
-        if (count($cajas) > 0 && !empty($cajas)) {
-
+        if (!is_null($cajas) && !empty($cajas) && count($cajas) > 0) {
+            $this->out = $this->respuesta_json('success', 200, 'Destinos encontrados.');
+            $this->out['cajas'] = $cajas;
         } else {
-
+            $this->out['message'] = 'No hay destinos registrados';
         }
 
-        return response()->json($cajas, 200);
+        return response()->json($this->out, $this->out['code']);
     }
 
     public function store(Request $request)
     {
-        //
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array) && count($params_array) > 0) {
+            $validacion = Validator::make($params_array,
+                [
+                    'descripcion' =>
+                        "required|unique:EMP_CAJAS,descripcion,NULL,NULL,id_destino,$params->id_destino,id_tipoCaja,$params->id_tipoCaja,id_distrib,$params->id_distrib",
+                    'peso_max' => 'required|between:0,99.99',
+                    'peso_min' => 'required|between:0,99.99',
+                    'peso_standard' => 'required|between:0,99.99',
+                    'id_destino' => ['required', 'exists:EMP_DESTINO,id'],
+                    'id_tipoCaja' => ['required', 'exists:EMP_TIPO_CAJA,id'],
+                    'id_distrib' => ['required', 'exists:EMP_DISTRIBUIDOR,id'],
+                    'id_codAllweights' => 'between:0,9999999'
+                ],
+                [
+                    'descripcion.unique' => 'La caja ya se encuentra registrada'
+                ]);
+
+            if ($validacion->fails()) {
+                $this->out['message'] = "Los datos enviados no son correctos";
+                $this->out['error'] = $validacion->errors();
+            } else {
+                $caja = new EMP_CAJA();
+                $caja->descripcion = $params_array['descripcion'];
+                $caja->peso_max = $params_array['peso_max'];
+                $caja->peso_min = $params_array['peso_min'];
+                $caja->peso_standard = $params_array['peso_standard'];
+                $caja->id_destino = $params_array['id_destino'];
+                $caja->id_tipoCaja = $params_array['id_tipoCaja'];
+                $caja->id_distrib = $params_array['id_distrib'];
+                $caja->id_codAllweights = $params_array['id_codAllweights'];
+                $caja->save();
+
+                $this->out = $this->respuesta_json('success', 200, 'Datos guardados correctamente');
+                $this->out['caja'] = $caja;
+            }
+
+        } else {
+            $this->out['message'] = "No se han recibido parametros";
+        }
+
+        return response()->json($this->out, $this->out['code']);
+
     }
 
     public function show($id)
     {
-        //
+        $caja = EMP_CAJA::find($id)->load(['destino', 'distribuidor', 'tipo_caja']);
+
+        if (is_object($caja) && !empty($caja)) {
+            $this->out = $this->respuesta_json('success', 200, 'Dato encontrado.');
+            $this->out['caja'] = $caja;
+        } else {
+            $this->out['message'] = 'No existen datos con el parametro enviado.';
+        }
+        return response()->json($this->out, $this->out['code']);
     }
 
     public function update(Request $request, $id)
