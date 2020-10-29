4c9a4def924bb5a93327b58fe880a22b85175087
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2020-10-29 12:20:46 -0500

    'correo'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -159,10 +159,10 @@ class UserController extends Controller
 
                 if (!empty($params->getToken)) {
                     $signup = $credentials;
+                } else {
+                    Mail::to($credentials['credential']->correo)
+                        ->send(new ConfirmacionAcceso($credentials['credential']));
                 }
-
-                Mail::to($credentials['credential']->correo)
-                    ->send(new ConfirmacionAcceso($credentials['credential']));
 
                 return response()->json($signup, 200);
             }
